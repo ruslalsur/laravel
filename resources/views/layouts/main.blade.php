0089bd6527e868847c3cc36c25338dad6d5cb2e4
rusla <suhrugen@gmail com>
2020-02-05 01:48:57 +0700

    тестирование работы компиляции sass и js через laravel.mix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,45 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/my-styles.css">
     <title>{{$title}}</title>
-    <style>
-        .menu > a {
-            color: #636b6f;
-            margin-right: 25px;
-            font-size: 14px;
-            font-weight: 600;
-            text-decoration: none;
-            text-transform: uppercase;
-            outline: none;
-            display: inline-flex;
-            transition: transform 250ms
-        }
-
-        a:hover {
-            color: red;
-            transform: scale(1.15)
-        }
-
-        a:active {
-            color: red;
-            transform: scale(1)
-        }
-
-        .hello {
-            font-size:36pt;
-            color: green;
-        }
-
-        .news {
-            font-size:36pt;
-            color: red;
-        }
-
-        .about {
-            font-size:36pt;
-            color: blue;
-        }
-    </style>
 </head>
 
 <body>
@@ -56,5 +19,6 @@
 
 @yield('content')
 
+<script src="js/my-js.js"></script>
 </body>
 </html>
