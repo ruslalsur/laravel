c3932b29a2e8e6d35ff05083813a6b0dd9b41c8f
rusla <suhrugen@gmail com>
2020-02-10 23:33:17 +0700

    добавил навигацию на странице выбранной категории

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,13 +4,13 @@
     <div class="content">
         <h4 class="mb-3">Новости по категории <strong>{{ $categoryName }}</strong></h4>
 
-        <div class="shadow p-3 mb-5 bg-white rounded">
+        <div class="shadow p-3 mb-3 bg-white rounded">
             @forelse($currentCatNews as $news)
                 <a class="nav-link font-weight-bolder" href="{{ route('newsOne', $news['id']) }}">{{ $news['title'] }}</a>
             @empty
                 <h5>в категории пока нет новостей</h5>
             @endforelse
-
         </div>
+        <a class="btn btn-outline-secondary btn-sm" href="{{ route('cat') }}">Вернуться к списку всех категорий</a>
     </div>
 @endsection
