6274509ddc4d4f7c0511316a85d5a94c7eef806d
rusla <suhrugen@gmail com>
2020-02-11 04:05:18 +0700

    перемещение кнопки назад в категорию в заголовок текущей категории, чтоьы она играла роли названия и возврата

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
     <div class="content">
-        <h4 class="mb-3">Новости по категории <strong>{{ $categoryName }}</strong></h4>
+        <h4 class="mb-3">Новости по категории <strong>{{ $categoryName }} </strong></h4>
 
         <div class="shadow p-3 mb-3 bg-white rounded">
             @forelse($currentCatNews as $news)
-                <a class="nav-link font-weight-bolder"
-                            href="{{ route('newsOne', $news['id']) }}"><span class="text-secondary ">{{ $news['date'] }}</span> {{ $news['title'] }}</a>
+                <a class="nav-link font-weight-bolder" href="{{ route('newsOne', $news['id']) }}">
+                    <span class="text-secondary ">{{ $news['date'] }}</span>
+                    {{ $news['title'] }}
+                </a>
             @empty
                 <h5>в категории пока нет новостей</h5>
             @endforelse
         </div>
-        <a class="btn btn-outline-secondary btn-sm" href="{{ route('cat') }}">Вернуться к списку всех категорий</a>
     </div>
 @endsection
--- resources/views/newsOne.blade.php
+++ resources/views/newsOne.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
     <div class="content">
-        <h4 class="mt-3 mb-3">Новость из категории <strong>{{ $categoryName }}</strong></h4>
+        <h4 class="mt-3 mb-3">Новость из категории
+            <a class="btn btn-secondary btn" href="{{ route('currentCat', $newsOne['category_id']) }}">
+                <span class="font-weight-bolder">{{ $categoryName }}</span>
+            </a> за {{ $newsOne['date'] }}
+        </h4>
 
         <div class="shadow-sm p-3 mb-3 bg-white rounded">
             <h5><strong>{{ $newsOne['title'] }}</strong></h5>
             <h6> {!! $newsOne['description'] !!} </h6>
         </div>
-        <a class="btn btn-outline-secondary btn-sm" href="{{ route('currentCat', $newsOne['category_id']) }}">Вернуться в категорию <strong>{{ $categoryName }}</strong></a>
     </div>
 @endsection
