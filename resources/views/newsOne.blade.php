f8d3c7f9b6efe351fca58acbaba867d4d25b7a05
rusla <suhrugen@gmail com>
2020-02-10 23:40:11 +0700

    прочитал, что можно так

--- resources/views/newsOne.blade.php
+++ resources/views/newsOne.blade.php
@@ -6,7 +6,7 @@
 
         <div class="shadow-sm p-3 mb-3 bg-white rounded">
             <h5><strong>{{ $newsOne['title'] }}</strong></h5>
-            <h6><?= $newsOne['description'] ?></h6>
+            <h6> {!! $newsOne['description'] !!} </h6>
         </div>
         <a class="btn btn-outline-secondary btn-sm" href="{{ route('currentCat', $newsOne['category_id']) }}">Вернуться в категорию <strong>{{ $categoryName }}</strong></a>
     </div>
