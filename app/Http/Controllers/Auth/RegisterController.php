88f8411498901b9d57f3c116c045be4ff298f6a2
rusla <suhrugen@gmail.com>
2020-03-18 04:22:02 +0700

    поиск ошибок процесс бесконечный, но уже пора сдавать

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'avatar' => 'storage/images/user.png',
             'password' => Hash::make($data['password']),
         ]);
     }
